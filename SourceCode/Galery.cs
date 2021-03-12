10cdb17f05d6b13a653a91d77caa83adbd11c816
Julian Pasqué <julian.pasque@xarvio.com>
2021-03-12 21:14:53 +0100

    backup

--- SourceCode/Galery.cs
+++ SourceCode/Galery.cs
@@ -1,15 +1,8 @@
-async void DoPickPhoto()
-        {
-            try
-            {
-                var photo = await MediaPicker.PickPhotoAsync();
+var photo = await MediaPicker.PickPhotoAsync();
 
-                await LoadPhotoAsync(photo);
+if(photo == null)
+{
+	Console.WriteLine("No image selected.");
 
-                Console.WriteLine($"PickPhotoAsync COMPLETED: {PhotoPath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"PickPhotoAsync THREW: {ex.Message}");
-            }
-        }
+}
+
