af937f2e87bf60f3167e9623421be250b1f89626
Julian Pasqué <julian.pasque@xarvio.com>
2021-04-06 22:33:52 +0200

    updates

--- SourceCode/ExampleClass-1.dart
+++ SourceCode/ExampleClass-1.dart
@@ -1,31 +1,18 @@
 class ExampleClass {
-  // Konstruktor
-  PublicExampleClass() {
-    PublicChar = String.fromCharCode(106);
-  }
-
-  // Ganzzahl
   int _PrivateInt;
 
-  // Gleitkommazahl
   double _PrivateDouble;
   double PublicFloat;
 
-  // Boolean
   bool _PrivateBool;
 
-  // Textzeichen
   String PublicChar;
 
-  // Zeichenfolge
   String PublicString;
 
-  // Datenfeld
   List<String> PublicCharArray;
 
-  // Auflistung
   List<int> PublicIntList;
 
-  // Hashtabelle
   Map<int, String> PublicHashTable;
 }
--- SourceCode/ExampleClass.cs
+++ SourceCode/ExampleClass.cs
@@ -1,22 +1,39 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace SampleApp.Models
 {
     public class PublicExampleClass
     {
+        // Konstruktor
+        public PublicExampleClass()
+        {
+            PublicChar = 'j';
+        }
+
+        // Ganzzahl
         private int PrivateInt;
 
+        // Gleitkommazahl
         private double PrivateDouble;
         public float PublicFloat;
 
+        // Boolean
         private bool PrivateBool;
-
+        
+        // Textzeichen
         public char PublicChar;
-
+        
+        // Zeichenfolge
         public string PublicString;
-
+        
+        // Datenfeld
         public char[] PublicCharArray;
-
+        
+        // Auflistung
+        public List<int> _PublicIntList;
+        
+        //Hashtabelle
         public Dictionary<int, char> PublicHashTable;
+
     }
 }
