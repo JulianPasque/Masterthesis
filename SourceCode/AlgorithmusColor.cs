e88bf884fbbaad2e90c0df367cbbba760fc409a3
Julian Pasqué <julian.pasque@xarvio.com>
2021-04-23 23:45:58 +0200

    updates

--- SourceCode/AlgorithmusColor.cs
+++ SourceCode/AlgorithmusColor.cs
@@ -2,10 +2,14 @@ public static string GetColor(string _Color)
 {
     var color = Color.FromName(_Color);
     if(color.IsKnownColor)
+    {
         return string.Format("{0:x6}", color.ToArgb());
-
+    }
+    
     if (_Color[0] == '#')
+    {
         _Color = _Color.Remove(0, 1);
+    }
 
     int res = 0;
     if (int.TryParse(_Color,
@@ -13,21 +17,26 @@ public static string GetColor(string _Color)
                 CultureInfo.InvariantCulture, out res))
     {
         if(_Color.Length == 3)
+        {
             return "ff" + _Color[0] + _Color[0] + 
                 _Color[1] + _Color[1] + 
                 _Color[2] + _Color[2];
-
+        }
         else if(_Color.Length == 4)
+        {
             return "" + _Color[0] + _Color[0] +
                 _Color[1] + _Color[1] + 
                 _Color[2] + _Color[2] + 
                 _Color[3] + _Color[3];
-
+        }
         else if(_Color.Length == 6)
+        {
             return "ff" + _Color;
-            
+        }
         else if(_Color.Length == 8)
+        {
             return _Color;
+        }
     }
     throw new ArgumentException("Could not identify a Color", _Color);
 }
--- SourceCode/Testcases.cs
+++ SourceCode/Testcases.cs
@@ -2,31 +2,27 @@
 public void TransformColorName()
 {
     Assert.IsTrue(S2SConverter.GetColor("White").Equals("ffffffff"));
+    Assert.IsTrue(S2SConverter.GetColor("Black").Equals("ff000000"));
+    Assert.IsTrue(S2SConverter.GetColor("Blue").Equals("ff0000FF"));
+    Assert.IsTrue(S2SConverter.GetColor("Black").Equals("ff000000"));
 }
+
 [Test]
-public void Transform3DigitColorWithHash()
+public void TransformColor()
 {
     Assert.IsTrue(S2SConverter.GetColor("#123").Equals("ff112233"));
     Assert.IsTrue(S2SConverter.GetColor("123").Equals("ff112233"));
-}
-[Test]
-public void Transform4DigitColorWithHash()
-{
+
     Assert.IsTrue(S2SConverter.GetColor("#1234").Equals("11223344"));
     Assert.IsTrue(S2SConverter.GetColor("1234").Equals("11223344"));
-}
-[Test]
-public void Transform6DigitColorWithHash()
-{
+
     Assert.IsTrue(S2SConverter.GetColor("#ffffff").Equals("ffffffff"));
     Assert.IsTrue(S2SConverter.GetColor("ffffff").Equals("ffffffff"));
-}
-[Test]
-public void Transform8DigitColorWithHash()
-{
+
     Assert.IsTrue(S2SConverter.GetColor("#ffffffff").Equals("ffffffff"));
     Assert.IsTrue(S2SConverter.GetColor("ffffffff").Equals("ffffffff"));
 }
+
 [Test]
 public void TransformInvalidTextTrowsException()
 {
@@ -38,4 +34,7 @@ public void TransformInvalidTextTrowsException()
 
     Assert.Throws<ArgumentException>(
     () => { S2SConverter.GetColor("#TestTest"); });
+
+     Assert.Throws<ArgumentException>(
+    () => { S2SConverter.GetColor(string.Empty); });
 }
